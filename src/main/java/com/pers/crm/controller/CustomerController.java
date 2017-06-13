ad7ae3090d9b33e4f1f16fbd0c8f4528a43bcdaa
stacywang0601 <stacywang61@gmail.com>
2017-06-13 12:40:50 -0400

    inject customerDao into customerController get customer using dao implementation

--- src/main/java/com/pers/crm/controller/CustomerController.java
+++ src/main/java/com/pers/crm/controller/CustomerController.java
@@ -1,15 +1,28 @@
 package com.pers.crm.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.pers.crm.dao.CustomerDAO;
+import com.pers.crm.entity.Customer;
+
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	// need to inject the customer dao
+	@Autowired
+	private CustomerDAO	customerDAO;
 
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel) {
+		// get customers from the dao
+		List<Customer> theCustomers = customerDAO.getCustomers();
+		// add the customers to the model
+		theModel.addAttribute("customers", theCustomers);
 
 		return "list-customers";
 	}
