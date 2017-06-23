e862ea945d46df03ccbc1a5ce06f7cc5a2e9f579
stacywang0601 <stacywang61@gmail.com>
2017-06-23 17:32:16 -0400

    add service and inject dao

---
+++ src/main/java/com/per/crm/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.per.crm.service;
+
+import java.util.List;
+
+import com.pers.crm.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+}
---
+++ src/main/java/com/per/crm/service/CustomerServiceImpl.java
@@ -0,0 +1,24 @@
+package com.per.crm.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.pers.crm.dao.CustomerDAO;
+import com.pers.crm.entity.Customer;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+	// need to inject customer dao
+	@Autowired
+	private CustomerDAO	customerDAO;
+
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		return customerDAO.getCustomers();
+	}
+}
