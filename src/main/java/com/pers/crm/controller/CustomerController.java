dc6fac216a64d0d496c68ce54fa58b43f295a866
stacywang0601 <stacywang61@gmail.com>
2017-06-23 17:34:58 -0400

    inject service in controller instead of dao

--- src/main/java/com/pers/crm/controller/CustomerController.java
+++ src/main/java/com/pers/crm/controller/CustomerController.java
@@ -8,20 +8,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.pers.crm.dao.CustomerDAO;
+import com.per.crm.service.CustomerService;
 import com.pers.crm.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
-	// need to inject the customer dao
+	// need to inject the customer service
 	@Autowired
-	private CustomerDAO	customerDAO;
+	private CustomerService	customerService;
 
 	@GetMapping("/list")
 	public String listCustomers(Model theModel) {
-		// get customers from the dao
-		List<Customer> theCustomers = customerDAO.getCustomers();
+		// get customers from the service
+		List<Customer> theCustomers = customerService.getCustomers();
 		// add the customers to the model
 		theModel.addAttribute("customers", theCustomers);
 
