df317cc88130924fdef1d3593b840be65235314e
stacywang0601 <stacywang61@gmail.com>
2017-06-23 20:11:12 -0400

    sort by last name

--- src/main/java/com/pers/crm/dao/CustomerDAOImpl.java
+++ src/main/java/com/pers/crm/dao/CustomerDAOImpl.java
@@ -22,8 +22,9 @@ public class CustomerDAOImpl implements CustomerDAO {
 
 		// get the current hibernate session
 		Session currentSession = sessionFactory.getCurrentSession();
-		// create a query
-		Query<Customer> theQuery = currentSession.createQuery("from Customer", Customer.class);
+		// create a query sort by last namet
+		Query<Customer> theQuery = currentSession.createQuery("from Customer order by lastName",
+		            Customer.class);
 		// execute query and get result list
 		List<Customer> customers = theQuery.getResultList();
 		// return the results
